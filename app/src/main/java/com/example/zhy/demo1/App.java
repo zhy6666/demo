268dd5f95655c593dace2a3fb9160b77929acc5d
zhy <17888804564@163.com>
2018-10-31 23:19:15 +0800

    'tusi'

--- app/src/main/java/com/example/zhy/demo1/App.java
+++ app/src/main/java/com/example/zhy/demo1/App.java
@@ -2,12 +2,15 @@ package com.example.zhy.demo1;
 
 import android.app.Application;
 
+import com.example.zhy.demo1.utils.MyToast;
+
 public class App extends Application {
 
     private static App mInstance;
     @Override
     public void onCreate() {
         super.onCreate();
+        MyToast.toast("jj");
         this.mInstance = this;
     }
 
